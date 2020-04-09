557f94b15e16ac1fa7e262729cfbc48e2421597e
baileyraerobbins <baileyrae.robbins@gmail.com>
2020-04-08 20:07:04 -0400

    adding all files

---
+++ app.js
@@ -0,0 +1,18 @@
+let friends = ["Rachel", "Bridget", "Maureen", "Dom", "Carlin"];
+loop1:
+for (let i = 0; i < [friends.length]; i++) {
+    loop2:
+    for (let n = 99; n >= 1; n--) {
+        //Rachel:
+        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
+        //Bridget:
+        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
+        //Maureen:
+        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
+        //Dom:
+        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
+        //Carlin:
+        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
+    }
+}
+
