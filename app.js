7f40c1f511fc26d4eba9489c2631f1c19c138b80
baileyraerobbins <baileyrae.robbins@gmail.com>
2020-04-10 16:15:42 -0400

    adding reviewed changes

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 let friends = ["Rachel", "Bridget", "Maureen", "Dom", "Carlin"];
-loop1:
-for (let i = 0; i < [friends.length]; i++) {
-    loop2:
-    for (let n = 99; n >= 1; n--) {
-        console.log(friends[i] + " drove down Highway " + n + " until dawn; she had nothing to lose; except for her shoes; now " + friends[i] + " is passed out on the lawn.");
-    }
-}
+// Rachel drove over 100 potholes by dawn; she thought she had nothing to lose; except for her shoes; now Rachel has 99 cars on the lawn.
+// Rachel drove over 2 potholes by dawn; she thought she had nothing to lose; except for her shoes; now Rachel has 1 car on the lawn.
+// Rachel drove over 1 pothole by dawn; she thought she had nothing to lose; except for her shoes; but now Rachel has no cars on the lawn.
 
+//outer loop for friends
+for (let i = 0; i < friends.length; i++) {
+    let friend = friends[i]
+    console.groupCollapsed(friend.toUpperCase() + ':');
+
+    //inner loop for song for each friend
+    for (let n = 99; n >= 1; n--) {
+        if (n === 1) {
+            console.log(
+                friend + " drove over " + n + " pothole by dawn; she thought she had nothing to lose; except for her shoes; now " +
+                friend + " has no more cars on the lawn."
+
+            );
+        } else if (n === 2) {
+            console.log(
+                friend + " drove over " + n + " potholes by dawn; she thought she had nothing to lose; except for her shoes; now " +
+                friend + " has " + (n - 1) + " car on the lawn."
+            );
+        } else {
+            console.log(
+                friend + " drove over " + n + " potholes by dawn; she thought she had nothing to lose; except for her shoes; now " +
+                friend + " has " + (n - 1) + " cars on the lawn."
+            );
+        }
+    }
+    console.groupEnd();
+}
