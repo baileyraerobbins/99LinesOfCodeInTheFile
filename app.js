7c00bda792bd18f31a820d509661fceefaed3423
baileyraerobbins <baileyrae.robbins@gmail.com>
2020-06-01 13:54:21 -0400

    adding final changes to files for review

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ function singButton() {
 
     function buttonClick() {
 
-        //Create div
-        let div = document.createElement("div");
-        div.className = "friend";
-
         //Add array of friends
         let friends = [
             "Rachel",
@@ -20,36 +16,52 @@ function singButton() {
             "Carlin"
         ];
 
-        //Loop song lyrics for each friend
+        //Loop friend names
         for (let i = 0; i < friends.length; i++) {
             let friend = friends[i]
+
+            //Create div
+            let div = document.createElement("div");
+            div.className = "friend";
+
+            //Create h3 for friend names and attach div
+            let h3 = document.createElement("h3");
+            let friendHeadings = document.createTextNode(friend + ":");
+            h3.appendChild(friendHeadings);
+            div.appendChild(h3);
+            document.body.appendChild(div);
+
+            //Loop song lyrics from 99 to 1 & for each friend
             for (let n = 99; n >= 1; n--) {
-                
-                // Rachel drove over 1 pothole by dawn; she thought she had nothing to lose; except for her shoes; but now Rachel has no more cars on the lawn.
+
+                //LAST SONG LYRIC: (Friend) drove over 1 pothole by dawn; she had nothing to lose; except for her shoes; but now (Friend) has no more cars on the lawn.
                 if (n === 1) {
                     let p1 = document.createElement("p");
                     let verseOne = document.createTextNode(
                         friend + " drove over " + n + " pothole by dawn; she had nothing to lose; except for her shoes; now " +
                         friend + " has no more cars on the lawn."
                     );
-                    
-                    //Attach paragraph to body
+
+                    //Attach paragraph to body & paragraph to div
                     p1.appendChild(verseOne);
                     document.body.appendChild(p1);
-                    
-                // Rachel drove over 2 potholes by dawn; she thought she had nothing to lose; except for her shoes; now Rachel has 1 car on the lawn. 
+                    div.appendChild(p1);
+
+                //SECOND TO LAST SONG LYRIC:(Friend) drove over 2 potholes by dawn; she had nothing to lose; except for her shoes; now (Friend) has 1 car on the lawn. 
                 } else if (n === 2) {
                     let p2 = document.createElement("p");
                     let verseTwo = document.createTextNode(
                         friend + " drove over " + n + " potholes by dawn; she had nothing to lose; except for her shoes; now " +
                         friend + " has " + (n - 1) + " car on the lawn."
                     );
-                    
-                    //Attach paragraph to body
+
+                    //Attach paragraph to body & paragraph to div
                     p2.appendChild(verseTwo);
                     document.body.appendChild(p2);
+                    div.appendChild(p2);
 
-                // Rachel drove over 100 potholes by dawn; she thought she had nothing to lose; except for her shoes; now Rachel has 99 cars on the lawn.
+
+                //FIRST SONG LYRICS TO FINAL: (Friend) drove over 99 potholes by dawn; she had nothing to lose; except for her shoes; now (Friend) has 98 cars on the lawn.
                 } else {
                     let p3 = document.createElement("p");
                     let verseThree = document.createTextNode(
@@ -57,10 +69,10 @@ function singButton() {
                         friend + " has " + (n - 1) + " cars on the lawn."
                     );
 
-                    //Attach paragraph to body
+                    //Attach paragraph to body & paragraph to div
                     p3.appendChild(verseThree);
                     document.body.appendChild(p3);
-
+                    div.appendChild(p3);
                 }
             }
         }
